de07cf111d3243ef0e26286c532fbde0416c1ad2
LauraLuzhen <ll.rodriguez@iesnervion.es>
2025-01-27 12:44:56 +0100

    Ingles

--- Tema3/src/collections2/Ej1.java
+++ Tema3/src/collections2/Ej1.java
@@ -1,8 +1,19 @@
 package collections2;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Ej1 {
 
 	public static void main(String[] args) {
 		
+		// Lectura y modificación de elementos
+		List<Integer> lista1 = new ArrayList<>();
+		
+		// Insercción y eliminación de elementos
+		List<Integer> lista2 = new LinkedList<>();
+		
+		lista1.getValue()
 	}
 }
---
+++ Tema3/src/english/Exercise1Collections.java
@@ -0,0 +1,17 @@
+package english;
+
+public class Exercise1Collections {
+
+	public static void main(String[] args) {
+		/*Set<Map.Entry<Integer, String>> entradas = conjunto.entrySet();
+		
+		for (Map.Entry<Integer, String> entrada: entradas) {
+			System.out.println(entrada.getKey() + " " + entrada.getValue());
+		}
+		
+		for (int clave: entradas) {
+			System.out.println(calve + " " + entradas.getValue(clave));
+		}
+		*/
+	}
+}
